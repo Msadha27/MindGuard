1ff1581f65bc9cd6b32aa4276eab71207d538080
Msadha27 <sadha272007@gmail.com>
2026-03-29 07:04:08 -0700

    MindGuard Mobile PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-icons: [
-  {
-    src: '/icon.png',
-    sizes: '192x192',
-    type: 'image/png',
-  }
-]
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
